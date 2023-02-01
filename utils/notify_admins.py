b538011f9cbd3d98244866cf5ee2acc7e96fefb1
Ilya <airscods@gmail.com>
2023-02-01 14:25:48 +0400

    fix: change name function on notify_admins.py

--- utils/notify_admins.py
+++ utils/notify_admins.py
@@ -5,7 +5,7 @@ from aiogram import Dispatcher
 from data.config import admins
 
 
-async def on_start_notify(dp: Dispatcher):
+async def on_startup_notify(dp: Dispatcher):
     for admin in admins:
         try:
             await dp.bot.send_message(admin, "Бот запущен и готов к работе")
