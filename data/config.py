2afedeff741e9b9a2562faab31d020c413aec5ef
Ilya <airscods@gmail.com>
2023-02-01 14:20:45 +0400

    update config.py

--- data/config.py
+++ data/config.py
@@ -5,3 +5,17 @@ from dotenv import load_dotenv
 load_dotenv()
 
 BOT_TOKEN = os.environ.get('TOKEN')
+admins = [
+
+]
+
+ip = os.getenv('ip')
+
+# aiogram.redis = {
+#     'host': ip,
+# }
+#
+# redis = {
+#     'address': (ip, 6379),
+#     'encoding': 'utf8'
+# }
