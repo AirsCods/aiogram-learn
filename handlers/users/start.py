df0f412f973863dd6aba2c9826df10191a269594
Ilya <airscods@gmail.com>
2023-02-01 15:55:03 +0400

    feat: add new handler for command /start

---
+++ handlers/users/start.py
@@ -0,0 +1,28 @@
+import re
+from aiogram import types
+from aiogram.dispatcher.filters import CommandStart
+from aiogram.utils.deep_linking import get_start_link
+
+from filters import IsPrivate
+from loader import dp
+
+
+@dp.message_handler(CommandStart(deep_link=re.compile(r'\d\d\d')), IsPrivate())
+async def bot_start(message: types.Message):
+    deep_link_args = message.get_args()
+    await message.answer(f'Привет, {message.from_user.full_name}!\n'
+                         f'Вы находитесь в личной переписке \n'
+                         f'В вашей команде есть диплинк \n'
+                         f'Вы передали аргумент {deep_link_args}')
+
+
+@dp.message_handler(CommandStart(), IsPrivate())
+async def bot_start(message: types.Message):
+    # bot_user = await dp.bot.me
+    # deep_link = f'https://t.me/{bot_user.username}?start=123'
+    deep_link = await get_start_link(payload='123')
+
+    await message.answer(f'Привет, {message.from_user.full_name}!\n'
+                         f'Вы находитесь в личной переписке \n'
+                         f'В вашей команде нет диплинка \n'
+                         f'Ваша диплинк ссылка - {deep_link}')
