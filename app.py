fc3f0402b2accf9a7125b5d7d724830e07360df1
Ilya <airscods@gmail.com>
2023-02-01 15:01:49 +0400

    update app.py

--- app.py
+++ app.py
@@ -1,19 +1,18 @@
 from utils.set_bot_commands import set_default_commands
+from aiogram import Dispatcher, executor
+from handlers import dp
+from utils.notify_admins import on_startup_notify
 
 
-async def on_startup(dp):
+async def on_startup(dispathcer: Dispatcher):
     # import filters
     # import middlewares
     # filters.setup(dp)
     # middlewares.setup(dp)
 
-    from utils.notify_admins import on_startup_notify
     await on_startup_notify(dp)
-    await set_default_commands(dp)
+    # await set_default_commands(dp)
 
 
 if __name__ == '__main__':
-    from aiogram import executor
-    from handlers import dp
-
     executor.start_polling(dp, on_startup=on_startup)
