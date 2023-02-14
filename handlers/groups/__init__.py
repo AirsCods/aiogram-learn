bc5110730166dc599cfee5b5fd5725795f9566dd
Ilya Samoletov <airscods@gmail.com>
2023-02-14 15:08:20 +0400

    feat: add group handlers for service message and set properties group

--- handlers/groups/__init__.py
+++ handlers/groups/__init__.py
@@ -1,3 +1,4 @@
+from .edit_chat import dp
 from .service_message import dp
 
 __all__ = ['dp']
--- handlers/groups/service_message.py
+++ handlers/groups/service_message.py
@@ -1,21 +1,20 @@
 from aiogram import types
 
+from filters import IsGroup
 from loader import dp
 
 
 # В группу добавился новый пользователь
-@dp.message_handler(content_types=types.ContentType.NEW_CHAT_MEMBERS)
+@dp.message_handler(IsGroup(), content_types=types.ContentType.NEW_CHAT_MEMBERS)
 async def new_member(message: types.Message):
-
     # Собираем в строку новых пользователей
     members = ', '.join([member.get_mention(as_html=True) for member in message.new_chat_members])
     await message.reply(f'Привет, {members}.')
 
 
 # Пользователь вышел или удален из группы
-@dp.message_handler(content_types=types.ContentType.LEFT_CHAT_MEMBER)
+@dp.message_handler(IsGroup(), content_types=types.ContentType.LEFT_CHAT_MEMBER)
 async def last_member(message: types.Message):
-
     # Пользователь вышел сам
     if message.left_chat_member.id == message.from_user.id:
         await message.answer(f'{message.left_chat_member.get_mention(as_html=True)} вышел из чата.')
